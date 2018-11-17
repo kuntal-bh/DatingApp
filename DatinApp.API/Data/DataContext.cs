e8723e35b7a0042f7f95da547be4974ebeddf191
kuntal <kntlbh@gmail.com>
2018-11-17 17:31:38 +0530

    initial commit

---
+++ DatinApp.API/Data/DataContext.cs
@@ -0,0 +1,16 @@
+using DatinApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatinApp.API.Data
+{
+    public class DataContext :DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Value> Values { get; set;}
+        
+    }
+}
