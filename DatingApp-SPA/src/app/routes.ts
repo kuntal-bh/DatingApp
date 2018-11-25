a63390f29514b6a165a732c436c0a2800b12f3f2
kuntal <kntlbh@gmail.com>
2018-11-25 17:38:31 +0530

    Another commit

--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -8,10 +8,16 @@ import { Routes } from '@angular/router';
 
 
 export const approutes: Routes = [
-    { path: 'home', component: HomeComponent },
-    { path: 'members', component: Member_listComponent , canActivate: [AuthGuard] },
-    { path: 'messages', component: MessagesComponent },
-    { path: 'lists', component: ListsComponent },
-    { path: '**', redirectTo: 'home' , pathMatch: 'full' },
+    { path: '', component: HomeComponent },
+    { path : '' ,
+      runGuardsAndResolvers : 'always',
+      canActivate: [AuthGuard],
+      children: [
+        { path: 'members', component: Member_listComponent , canActivate: [AuthGuard] },
+        { path: 'messages', component: MessagesComponent },
+        { path: 'lists', component: ListsComponent },
+      ]
+    },
+    { path: '**', redirectTo: '' , pathMatch: 'full' },
 ];
 
